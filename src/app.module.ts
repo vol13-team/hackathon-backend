e3845a4db94c5ed567227b3a033352a31155030e
taku10101 <taku1011ht@gmail.com>
2024-01-12 01:32:47 +0900

    prisma add

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { UserController } from './user/user.controller';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, PrismaModule],
   controllers: [AppController, AuthController, UserController],
   providers: [AppService, AuthService, UserService],
 })
