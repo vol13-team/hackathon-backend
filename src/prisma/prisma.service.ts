c8381452705bede126a94f8e94fa2cad492b50cd
taku10101 <taku1011ht@gmail.com>
2024-01-12 02:53:01 +0900

    service add

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+//DB接続ロジックを定義
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get<string>('DATABASE_URL'),
+        },
+      },
+    });
+  }
+}
