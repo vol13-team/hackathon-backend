3669cf484e6fde593aa2c6dd954e29119a166b1b
taku10101 <taku1011ht@gmail.com>
2024-01-13 02:34:07 +0900

    data format done

--- src/qiita_api/qiita_api.service.ts
+++ src/qiita_api/qiita_api.service.ts
@@ -5,7 +5,14 @@ export class QiitaApiService {
   async getPickUp(page: number) {
     const url = `https://qiita.com/api/v2/items?pickup=true&per_page=${page}`;
     const response = await fetch(url);
-    const data = await response.json();
+    const jsonData = await response.json();
+    const data = jsonData.map((item) => {
+      return {
+        title: item.title,
+        url: item.url,
+        user: item.user.id,
+      };
+    });
     return data;
   }
 }
