a59ee1b701f70e23e3aff1ffbc8d91d8d4ce4aca
taku10101 <taku1011ht@gmail.com>
2024-01-13 02:19:32 +0900

    Service add

--- src/qiita_api/qiita_api.service.ts
+++ src/qiita_api/qiita_api.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class QiitaApiService {}
+export class QiitaApiService {
+  getPickUp(page: number) {
+    fetch(`https://qiita.com/api/v2/items?pickup=true&per_page=${page}`);
+  }
+}
