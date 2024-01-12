cc6225fa8b55f734008680a3ee8e31720eb2feb4
taku10101 <taku1011ht@gmail.com>
2024-01-13 02:15:39 +0900

    miss

--- src/qiita_api/qiita_api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QiitaApiController } from './qiita_api.controller';
-import { QiitaApiService } from './qiita_api.service';
-
-@Module({
-  controllers: [QiitaApiController],
-  providers: [QiitaApiService]
-})
-export class QiitaApiModule {}
