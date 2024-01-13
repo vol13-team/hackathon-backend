2855319d46f89b4f14f21a999d2dbac0123bf46e
taku10101 <taku1011ht@gmail.com>
2024-01-13 17:05:10 +0900

    user token add

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  create() {
+    return 'This action adds a new user';
+  }
+}
