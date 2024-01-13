77dd825eabb6096b1f0875d6f05c37f250a61456
taku10101 <taku1011ht@gmail.com>
2024-01-13 20:59:59 +0900

    dto 引数 fix

--- src/issue/issue.controller.ts
+++ src/issue/issue.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { IssueService } from './issue.service';
+import { CreateIssueDto, CreateOptionsDto } from './dto';
 
 @Controller('issue')
 export class IssueController {
@@ -15,5 +16,13 @@ export class IssueController {
   @Get('issue/:id')
   findByIssue(@Param('id') id: string) {
     return this.issueService.findByIssue(id);
+  }
+  @Post('/:articleId')
+  create(
+    @Param('articleId') articleId: string,
+    @Body() issueDto: CreateIssueDto,
+    optionsDto: CreateOptionsDto,
+  ) {
+    return this.issueService.create(articleId, issueDto, optionsDto);
   }
 }
