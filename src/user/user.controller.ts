a6fcae4ed7c33b32c22dbba7bdcb6bcc39fd39b9
taku10101 <taku1011ht@gmail.com>
2024-01-13 16:53:37 +0900

    end point ad

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  //   @Get()
+  //   findOne() {
+  //     return this.userService.findOne();
+  //   }
+
+  @Post()
+  create() {
+    return this.userService.create();
+  }
+}
