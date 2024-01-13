6b6d068a519a097be205c57687fac6de823a5ed4
taku10101 <taku1011ht@gmail.com>
2024-01-13 18:23:34 +0900

    get  Service add

--- src/issue/issue.service.ts
+++ src/issue/issue.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class IssueService {}
+export class IssueService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll() {
+    try {
+      const issues = await this.prismaService.issue.findMany();
+      return issues;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async findByIssue(id: string) {
+    try {
+      const issue = await this.prismaService.issue.findUnique({
+        where: { IssueID: id },
+      });
+      return issue;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
