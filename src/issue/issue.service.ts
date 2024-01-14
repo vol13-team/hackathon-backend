6b52324610a5ce45f37d4d4a944f5e6d980d1cb5
taku10101 <taku1011ht@gmail.com>
2024-01-14 15:31:05 +0900

    options in issue fix  Service

--- src/issue/issue.service.ts
+++ src/issue/issue.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateIssueDto, CreateOptionsDto } from './dto';
+import { CreateIssueDto } from './dto';
 
 @Injectable()
 export class IssueService {
@@ -35,27 +35,7 @@ export class IssueService {
     }
   }
 
-  async createOption(optionsDto: CreateOptionsDto) {
-    try {
-      const options = await this.prismaService.issueOptions.create({
-        data: {
-          Option1: optionsDto.option1,
-          Option2: optionsDto.option2,
-          Option3: optionsDto.option3,
-          Option4: optionsDto.option4,
-          CorrectOption: optionsDto.correctOption,
-        },
-      });
-      return options;
-    } catch (error) {
-      throw error;
-    }
-  }
-  async createIssue(
-    issueDto: CreateIssueDto,
-    articleId: string,
-    optionId: string,
-  ) {
+  async create(issueDto: CreateIssueDto, articleId: string) {
     try {
       const issue = await this.prismaService.issue.create({
         data: {
@@ -63,32 +43,16 @@ export class IssueService {
           IssueDetail: issueDto.issueDetail,
           Visibility: issueDto.visibility,
           explanation: issueDto.explanation,
+          Option1: issueDto.option1,
+          Option2: issueDto.option2,
+          Option3: issueDto.option3,
+          Option4: issueDto.option4,
+          CorrectOption: issueDto.correctOption,
           PostUserID: issueDto.postUserID,
           ArticleID: articleId,
-          OptionsID: optionId,
         },
       });
       return issue;
-    } catch (error) {
-      throw error;
-    }
-  }
-  async create(
-    articleId: string,
-    issueDto: CreateIssueDto,
-    optionsDto: CreateOptionsDto,
-  ) {
-    try {
-      const options = await this.createOption(optionsDto);
-      const optionId = options.OptionID;
-      if (!optionId) {
-        throw new Error('option not found');
-      }
-      const issue = await this.createIssue(issueDto, articleId, optionId);
-      return {
-        options,
-        issue,
-      };
     } catch (error) {
       throw error;
     }
