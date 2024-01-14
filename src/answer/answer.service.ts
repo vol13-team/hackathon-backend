59275e882ee11d85bdf222b3c26ff3f3362e5652
taku10101 <taku1011ht@gmail.com>
2024-01-15 02:05:30 +0900

    logic memo add

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -4,7 +4,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class AnswerService {
   constructor(private readonly prismaService: PrismaService) {}
-  async verifying() {
-    //
-  }
+  //create verifying
+  //issue id,select optionを受け取る
+  //issue idからissue_collectionを取得
+  //issue id===issue_collectionの場合decision = true
+  //issue id!==issue_collectionの場合decision = false
+
+  //get verifying
+  //answer_idを受け取る
+  //answer_idからanswerを取得
+  //issueからcollectOption,explanationを取得
+
+  async verifying() {}
 }
