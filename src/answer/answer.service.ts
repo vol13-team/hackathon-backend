8065c0dd75ee35252b7a626df0fcc09a71148c7e
taku10101 <taku1011ht@gmail.com>
2024-01-15 03:57:21 +0900

    get  Service add

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -7,14 +7,21 @@ export class AnswerService {
   //create verifying
   //issue id,select optionを受け取る
   //issue idからissue_collectionを取得
-  //issue id===issue_collectionの場合decision = true
-  //issue id!==issue_collectionの場合decision = false
+  //issue id===issue_collectionの場合decision = true登録
+  //issue id!==issue_collectionの場合decision = false登録
 
   //get verifying
   //answer_idを受け取る
   //answer_idからanswerを取得
   //issueからcollectOption,explanationを取得
-
+  async finedAll() {
+    return this.prismaService.answer.findMany();
+  }
+  async findOne(answerId: string) {
+    return this.prismaService.answer.findUnique({
+      where: { AnswerID: answerId },
+    });
+  }
   async create(dto: CreateAnswerDto, issueId: string) {
     console.log(dto, issueId);
   }
