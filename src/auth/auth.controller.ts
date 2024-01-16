6816f5dc7b4e801beff51e0709a53c9946e0a8f8
taku10101 <taku1011ht@gmail.com>
2024-01-16 14:03:50 +0900

    auth endpoint add

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dto';
+import { RefreshJwtGuard } from './guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+
+  @Post('login')
+  async login(@Body() dto: CreateUserDto) {
+    return this.authService.login(dto);
+  }
+
+  @UseGuards(RefreshJwtGuard)
+  @Post('refresh')
+  async refreshToken(@Request() req) {
+    console.log('refreshed');
+
+    return await this.authService.refreshToken(req.user);
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto';
 
@@ -18,15 +10,11 @@ export class UserController {
   findAll() {
     return this.userService.findAll();
   }
-  @Get('/:id')
+  @Get('/:user_id')
   findOne(@Param('user_id') user_id: string) {
     return this.userService.findOne(user_id);
   }
 
-  @Post()
-  create(@Body() dto: CreateUserDto) {
-    return this.userService.create(dto);
-  }
   @Put('/:user_id')
   update(@Param('user_id') user_id: string, @Body() dto: CreateUserDto) {
     return this.userService.update(user_id, dto);
