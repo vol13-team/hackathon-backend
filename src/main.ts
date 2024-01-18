1508380cb8d6a6d0480a98e59f3bb5c6005e5d61
taku10101 <taku1011ht@gmail.com>
2024-01-19 01:21:43 +0900

    port del

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     origin: '*',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
