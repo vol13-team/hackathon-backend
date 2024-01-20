5a4de424966e9f5c18162dcf72df85b6b97ac890
Takumi <fd3s.bcnr33@gmail.com>
2024-01-21 01:42:49 +0900

    Article Help

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAnswerDto } from './dto';
+import { IssueService } from 'src/issue/issue.service';
 @Injectable()
 export class AnswerService {
-  constructor(private readonly prismaService: PrismaService) {}
-  //create verifying
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly issueService: IssueService,
+  ) {} //create verifying
   //issue id,select optionを受け取る
   //issue idからissue_collectionを取得
   //issue id===issue_collectionの場合decision = true登録
@@ -17,6 +20,7 @@ export class AnswerService {
   async finedAll() {
     return this.prismaService.answer.findMany();
   }
+
   async finedByAnswerID(answerId: string) {
     return this.prismaService.answer.findUnique({
       where: { answer_id: answerId },
@@ -32,12 +36,34 @@ export class AnswerService {
   async create(dto: CreateAnswerDto, issueId: string) {
     const answer = await this.prismaService.answer.create({
       data: {
-        answer_user_id: dto.answer_user_id,
-        select_option: dto.select_option_id,
+        answer_user: {
+          connect: {
+            user_id: dto.answer_user_id,
+          },
+        },
+        issue: {
+          connect: {
+            issue_id: issueId,
+          },
+        },
+        select_option_id: dto.select_option_id,
         decision: dto.decision,
-        issue_id: issueId,
       },
     });
+
     return answer;
+  }
+
+  async answerVerifying(answer_id: string) {
+    const answer = await this.finedByAnswerID(answer_id);
+    const issue = await this.issueService.findByIssue(answer.issue_id);
+    const selectOption = await answer.select_option_id; //選択した選択肢
+    const collectOption = issue.correct_option;
+
+    //選択した選択肢と正解の選択肢が一致しているならtrue
+    if (selectOption === collectOption) {
+      return true;
+    }
+    return false;
   }
 }
--- src/answer/dto/answer.dto.ts
+++ src/answer/dto/answer.dto.ts
@@ -2,4 +2,5 @@ export class CreateAnswerDto {
   select_option_id: string;
   decision: boolean;
   answer_user_id: string;
+  post_user_id: string;
 }
